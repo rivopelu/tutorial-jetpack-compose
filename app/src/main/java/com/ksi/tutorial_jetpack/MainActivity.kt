5b0d196db246554740db4a78db453537ad5dba9e
rivo <arvind.jahlani@forkshape.com>
2023-12-02 01:10:28 +0800

    3-topbar

--- app/src/main/java/com/ksi/tutorial_jetpack/MainActivity.kt
+++ app/src/main/java/com/ksi/tutorial_jetpack/MainActivity.kt
@@ -3,11 +3,23 @@ package com.ksi.tutorial_jetpack
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.ksi.tutorial_jetpack.ui.theme.TutorialjetpackTheme
@@ -22,25 +34,61 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ScaffoldSample()
                 }
             }
         }
     }
+
+    @Composable
+    fun CheckingColumn() {
+        Column {
+            Text(text = "INI TEXT");
+            Text(text = "INI TEXT");
+            Row {
+                Text(text = "1")
+                Text(text = "1")
+                Text(text = "1")
+            }
+        }
+    }
+
+    @Composable
+    fun Greeting(name: String, modifier: Modifier = Modifier) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun GreetingPreview() {
+        TutorialjetpackTheme {
+            Greeting("Android")
+        }
+    }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TutorialjetpackTheme {
-        Greeting("Android")
+fun ScaffoldSample(){
+    Scaffold(topBar =  {
+        TopAppBar(title = { Text(text = "Testing Compose")})
+    },
+        floatingActionButton = {
+            FloatingActionButton(onClick = { /*TODO*/ }) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = null)
+            }
+        }
+    ) {
+        Box(modifier = Modifier
+            .padding(it)
+            .fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ){
+            Text(text = "HELLO COMPOSE")
+        }
+        
     }
 }
